bea2f24261362c3a13b5f9a6e06bef47775db94b
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2020-01-15 12:12:20 -0500

    Add getGoGoDays API

--- ts_src/index.spec.ts
+++ ts_src/index.spec.ts
@@ -1,5 +1,6 @@
 import {
 	isGoGoDay,
+	getGoGoDays,
 } from '.';
 
 import 'mocha';
@@ -9,5 +10,17 @@ describe( 'isGoGoDay', () => {
 	it( 'should work for any given dates', () => {
 		expect( isGoGoDay( new Date( '2019-12-24T00:00:00-05:00' ) ) ).to.be.false;
 		expect( isGoGoDay( new Date( '2019-12-25T00:00:00-05:00' ) ) ).to.be.true;
+	} );
+} );
+
+describe( 'getGoGoDays', () => {
+	it( 'should list all go go days for a given year', () => {
+		expect( getGoGoDays( 2020 ) ).to.have.lengthOf( 36 );
+		expect( getGoGoDays( 2020 )[0] ).to.equal( '2020-01-05' );
+		expect( getGoGoDays( 2020 )[35] ).to.equal( '2020-12-25' );
+	} );
+
+	it( 'should throw an error', () => {
+		expect( () => getGoGoDays( 1999 ) ).to.throw( Error );
 	} );
 } );
--- ts_src/index.ts
+++ ts_src/index.ts
@@ -1,5 +1,54 @@
+const GO_GO_DAYS: Array<string> = [
+	'01-05',
+	'01-15',
+	'01-25',
+	'02-05',
+	'02-15',
+	'02-25',
+	'03-05',
+	'03-15',
+	'03-25',
+	'04-05',
+	'04-15',
+	'04-25',
+	'05-05',
+	'05-15',
+	'05-25',
+	'06-05',
+	'06-15',
+	'06-25',
+	'07-05',
+	'07-15',
+	'07-25',
+	'08-05',
+	'08-15',
+	'08-25',
+	'09-05',
+	'09-15',
+	'09-25',
+	'10-05',
+	'10-15',
+	'10-25',
+	'11-05',
+	'11-15',
+	'11-25',
+	'12-05',
+	'12-15',
+	'12-25',
+];
+
 const isGoGoDay = ( date: Date = new Date() ) => date.getDate().toString().endsWith( '5' );
 
+const getGoGoDays = ( year: number ) => {
+	// Expect a reasonable year. This code will die.
+	if ( year < 2000 || year >= 2100 ) {
+		throw new Error( 'Please provide a year in this century.' );
+	}
+
+	return GO_GO_DAYS.map( it => `${year}-${it}` );
+};
+
 export {
+	getGoGoDays,
 	isGoGoDay,
 };
