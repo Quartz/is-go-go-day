d37ca8638226814587d8044828d02113a351e302
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2020-02-10 10:33:59 -0500

    Clean up

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,32 @@
 module.exports = {
 	env: {
-	  browser: false,
-	  commonjs: true,
-	  es6: true,
-	  node: true
+		browser: false,
+		commonjs: true,
+		es6: true,
+		node: true
 	},
 	extends: [
-	  '@quartz/eslint-config-base'
+		'@quartz/eslint-config-base'
 	],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
-	  ecmaVersion: 2018
+		ecmaVersion: 2018
 	},
 	plugins: [
-	  '@typescript-eslint'
+		'@typescript-eslint'
 	],
 	settings: {
 		"import/resolver": {
-		  node: {
+			node: {
 			extensions: [".js", ".jsx", ".ts", ".tsx"]
-		  }
-	  },
+			}
+		},
 	},
 	overrides: [{
-	  files: [ "*.ts" ],
-	  rules: {
-		  "no-unused-vars": 0,
-		  "jest/valid-expect": 0,
-	  }
+		files: [ "*.ts" ],
+		rules: {
+			"no-unused-vars": 0,
+			"jest/valid-expect": 0,
+		}
 	}]
 };
